cab039ebf0241e639a1d136a9efabbce69376a24
brickstar <brickstar@gmail.com>
2018-01-28 22:34:54 -0700

    updated

--- lib/round.rb
+++ lib/round.rb
@@ -2,17 +2,32 @@ class Round
   attr_reader :deck,
               :guesses
 
+
   def initialize(deck)
     @deck         = deck
     @guesses      = []
-    @current_card = 0
+    @card_index   = 0
+
   end
 
   def current_card
-    @deck.cards[@current_card]
+    @deck.cards[@card_index]
   end
 
-  def record_guess(response)
-    
+  def record_guess(user_answer)
+    guess = Guess.new(user_answer, current_card)
+    @guesses << guess
+    guess
   end
+
+  def number_correct
+    count = 0
+    @guesses.each do |guess|
+      if guess.correct?
+        count += 1
+      end
+    end
+    count
+  end
+
 end
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -6,47 +6,36 @@ require "pry"
 
 class GuessTest < Minitest::Test
 
-  def test_guess_exists
-    card  = Card.new("What is the capital of Alaska?", "Juneau")
-    guess = Guess.new("Juneau", card)
+  def setup
+    @card  = Card.new("What is the capital of Alaska?", "Juneau")
+    @guess = Guess.new("Juneau", @card)
+  end
 
-    assert_instance_of Guess, guess
+  def test_guess_exists
+    assert_instance_of Guess, @guess
   end
 
   def test_guess_has_a_card
-    card  = Card.new("What is the capital of Alaska?", "Juneau")
-    guess = Guess.new("Juneau", card)
-
-    assert_instance_of Card, guess.card
+    assert_instance_of Card, @guess.card
   end
 
   def test_guess_has_a_response
-    card  = Card.new("What is the capital of Alaska?", "Juneau")
-    guess = Guess.new("Juneau", card)
-
-    assert_equal "Juneau", guess.response
+    assert_equal "Juneau", @guess.response
   end
 
   def test_guess_is_correct
-    card  = Card.new("What is the capital of Alaska?", "Juneau")
-    guess = Guess.new("Juneau", card)
-
-    assert true, guess.correct?
+    assert true, @guess.correct?
   end
 
   def test_feedback_is_correct
-    card  = Card.new("What is the capital of Alaska?", "Juneau")
-    guess = Guess.new("Juneau", card)
-
-    assert_equal "Correct!", guess.feedback
+    assert_equal "Correct!", @guess.feedback
   end
 
   def test_another_card
-    card  = Card.new("Which planet is closest to the sun?", "Mercury")
+    card  = Card.new("blah", "Saturn")
     guess = Guess.new("Saturn", card)
-
     assert_instance_of Card, guess.card
     assert_equal "Saturn", guess.response
-    refute guess.correct?
+    assert guess.correct?
   end
 end
